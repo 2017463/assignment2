c6bf1a90d3daa12f157e0010026638385ad65a71
2017463 <73094946+2017463@users.noreply.github.com>
2020-11-02 11:02:52 +0500

    Update App.js

--- assign2/src/App.js
+++ assign2/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           Hamza Jahangir (2017463)
+           hello Hamza Jahangir (2017463)
         </p>
         <a
           className="App-link"
